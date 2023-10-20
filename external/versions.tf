31f393ed77eaf35633696b7186b7d6d1f9c1bd43
donnie-darko <2626886+donnie-darko@users.noreply.github.com>
2023-10-20 23:39:18 +0200

    init

--- external/versions.tf
+++ external/versions.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "remote" {
     hostname     = "app.terraform.io"
-    organization = "khuedoan"
+    organization = "aki72ef2ab51"
 
     workspaces {
       name = "homelab-external"
