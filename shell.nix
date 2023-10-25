658a1bdc885ea809169763f27e466b62d3646b86
donnie-darko <2626886+donnie-darko@users.noreply.github.com>
2023-10-25 15:42:46 +0300

    try new nixos version

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
-# https://status.nixos.org (nixos-22.11)
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/6c591e7adc51.tar.gz") {} }:
+# https://status.nixos.org (nixos-23.05)
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz") {} }:
 
 let
   python-packages = pkgs.python3.withPackages (p: with p; [
